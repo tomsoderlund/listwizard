dc7bb35df760805618b8b4b95418d7871f308359
Tom Söderlund <tom@tomsoderlund.com>
2020-02-08 21:45:30 +0100

    progress-button

--- src/components/CopyToClipboard.svelte
+++ src/components/CopyToClipboard.svelte
@@ -1,16 +1,26 @@
-<button on:click={copyToClipboard}>Copy to clipboard</button>
+<button
+  on:click={copyToClipboard}
+  on:mouseover={() => { isDone = false }}
+  class={'progress-button' + (inProgress ? ' in-progress' : '') + (isDone ? ' done' : '')}
+>Copy to clipboard</button>
 
 <script>
 
   export let textToCopy
+  let isDone = false
+  let inProgress = false
 
   const copyToClipboard = async () => {
+    isDone = false
+    inProgress = true
     if (!window.navigator.clipboard || !textToCopy) return
     try {
       await window.navigator.clipboard.writeText(textToCopy)
-      window.alert('Copied to clipboard')
+      isDone = true
     } catch (err) {
       console.error('Failed to copy!', err)
+    } finally {
+      inProgress = false      
     }
   }
   
