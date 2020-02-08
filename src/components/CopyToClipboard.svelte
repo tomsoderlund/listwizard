cae0161cbe9f7b1dc4f6d470d9a37ca61a45f0b5
Tom Söderlund <tom@tomsoderlund.com>
2020-02-08 11:02:30 +0100

    Update CopyToClipboard.svelte

--- src/components/CopyToClipboard.svelte
+++ src/components/CopyToClipboard.svelte
@@ -5,9 +5,9 @@
   export let textToCopy
 
   const copyToClipboard = async () => {
-    if (!navigator.clipboard || !textToCopy) return
+    if (!window.navigator.clipboard || !textToCopy) return
     try {
-      await navigator.clipboard.writeText(textToCopy)
+      await window.navigator.clipboard.writeText(textToCopy)
       window.alert('Copied to clipboard')
     } catch (err) {
       console.error('Failed to copy!', err)
