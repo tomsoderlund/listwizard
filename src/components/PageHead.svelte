0c4558003f3f72949f62c2893de4b1afb7ba1467
Tom Söderlund <tom@tomsoderlund.com>
2020-02-08 21:58:09 +0100

    og:image

--- src/components/PageHead.svelte
+++ src/components/PageHead.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { stores } from '@sapper/app'
   import { description as defaultDescription } from '../../package.json'
+
+  const { page } = stores() // preloading, page, session
+  const { host, path } = $page // host, path, params, query
 
   export let title = undefined
   export let description = defaultDescription
@@ -8,4 +12,5 @@
 <svelte:head>
   <title>{title ? title + ' – ' : ''}Listwizard – {description}</title>
   <meta name="description" content={description} />
+  <meta property="og:image" content={`https://d3htstcz5zldi1.cloudfront.net/?url=${host + path}`} />
 </svelte:head>
