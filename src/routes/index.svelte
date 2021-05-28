e8171f86b8935d2430382737b932b81b8c3ccca8
Tom Söderlund <tom@tomsoderlund.com>
2021-05-28 20:51:26 +0200

    Center start page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,27 +1,33 @@
 <PageHead />
 
-<h1>{nameCapitalized}</h1>
+<div class='content'>
+  <h1>{nameCapitalized}</h1>
 
-<p>{description}</p>
+  <p>{description}</p>
 
-<section>
-  <a href='./steps' class='button'>Step-by-Step Manipulation</a>
-	<a href='./boolean' class='button'>Boolean Manipulation</a>
-</section>
+  <section>
+    <a href='./steps' class='button'>Step-by-Step Manipulation</a>
+    <a href='./boolean' class='button'>Boolean Manipulation</a>
+  </section>
 
-<p>&copy; <a href="https://twitter.com/tomsoderlund" target="_blank">Tom S&ouml;derlund</a> 2020</p>
+  <p>&copy; <a href="https://twitter.com/tomsoderlund" target="_blank">Tom S&ouml;derlund</a> 2020</p>
+</div>
 
 <script>
-	import { name, description } from '../../package.json'
+  import { name, description } from '../../package.json'
 
   import PageHead from '../components/PageHead.svelte'
 
-	const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1)
+  const capitalizeFirstLetter = str => str.charAt(0).toUpperCase() + str.slice(1)
 
   const nameCapitalized = capitalizeFirstLetter(name)
 </script>
 
 <style>
+  .content {
+    text-align: center;
+  }
+
   .button {
     font-size: 1.5em;
     width: 10em;
