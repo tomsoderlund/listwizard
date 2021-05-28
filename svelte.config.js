c951dd35188f4241ad44f20ad4c7836ebbbff78d
Tom Söderlund <tom@tomsoderlund.com>
2021-05-28 20:26:51 +0200

    @sveltejs/adapter-vercel@next

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,12 @@
 /** @type {import('@sveltejs/kit').Config} */
+const vercel = require('@sveltejs/adapter-vercel');
+
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
-	}
+	},
+  adapter: vercel()
 };
 
 export default config;
